a27d7a452532ccb667cff5bc972f9e619d2c6f74
AswiniC09 <113245187+AswiniC09@users.noreply.github.com>
2022-11-08 15:06:49 +0530

    Update demo.py

--- INTEGRATING SENDGRID SERVICE/demo.py
+++ INTEGRATING SENDGRID SERVICE/demo.py
@@ -1,1 +1,14 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
+sg = sendgrid.SendGridAPIClient(api_key= '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' )
+from_email = Email("itaswinic@gmail.com")
+to_email = To("aswinichittibabu05@gmail.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
